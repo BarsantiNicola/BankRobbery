eb0909d222285baa8c10ea0399afe539f76812df
BarsantiNicola <barsantinicola9@hotmail.it>
2020-06-04 15:56:39 +0200

    Create funzioni di plot e stampati grafici

--- risultati/util.py
+++ risultati/util.py
@@ -12,7 +12,7 @@ def getDatas( filename , varname ):
     fl = f.readlines()
     values = []
     for x in fl :
-        if x.find(varname,15,40) != -1:
+        if x.find(varname,18,50) == 18:
             values.append(x.split(","));
     f.close();
     return values;
@@ -31,15 +31,130 @@ def simplePlot(datas , linecolor):
     plt.plot(times,vals)
     plt.plot(times,low, color=linecolor)
     plt.plot(times,up, color=linecolor)
+    
+def phisicalPlot():
+    parameters = getPhisicalVars()
+    var_values = []
+    vals = []
+    mean = []
+    up = []
+    down = []
+    all_data = []
+    time = []
+    for param in parameters:
+        vals = getDatas("PhisicalAttack_results.csv",param)
+        for x in vals:
+            time.append(float(x[3]))
+            mean.append(float(x[5]))
+            down.append(float(x[5])-float(x[7]))
+            up.append(float(x[5])+float(x[7]))
+        all_data.append(time)
+        all_data.append(mean)
+        all_data.append(up)
+        all_data.append(down)
+        var_values.append(all_data)
+        time = []
+        mean = []
+        up = []
+        down = []
+        all_data = []
+    for i in range(0,len(var_values)):
+        #plt.plot(var_values[i][0],var_values[i][1], label=parameters[i])
+        plt.plot(var_values[i][0],var_values[i][2] , label=parameters[i])
+        #plt.plot(var_values[i][0],var_values[i][3])
+    plt.legend()
+    plt.savefig("PhisicalPlot", quality=95, dpi=1000, facecolor='w', edgecolor='w',
+        orientation='portrait', papertype=None, format=None,
+        transparent=False, bbox_inches=None, pad_inches=0.1,
+        frameon=None, metadata=None)
+    
+    
+def cyberPlot():
+    parameters = getCyberVars()
+    var_values = []
+    vals = []
+    mean = []
+    up = []
+    down = []
+    all_data = []
+    time = []
+    for param in parameters:
+        vals = getDatas("CyberAttack_results.csv",param)
+        for x in vals:
+            time.append(float(x[3]))
+            mean.append(float(x[5]))
+            down.append(float(x[5])-float(x[7]))
+            up.append(float(x[5])+float(x[7]))
+        all_data.append(time)
+        all_data.append(mean)
+        all_data.append(up)
+        all_data.append(down)
+        var_values.append(all_data)
+        time = []
+        mean = []
+        up = []
+        down = []
+        all_data = []
+    for i in range(0,len(var_values)):
+        #plt.plot(var_values[i][0],var_values[i][1], label=parameters[i])
+        plt.plot(var_values[i][0],var_values[i][2] , label=parameters[i])
+        #plt.plot(var_values[i][0],var_values[i][3])
+    plt.legend()
+    plt.savefig("CyberPlot", quality=95, dpi=1000, facecolor='w', edgecolor='w',
+        orientation='portrait', papertype=None, format=None,
+        transparent=False, bbox_inches=None, pad_inches=0.1,
+        frameon=None, metadata=None)
+    
+def generalPlot():
+    parameters = getAllVars()
+    var_values = []
+    vals = []
+    mean = []
+    up = []
+    down = []
+    all_data = []
+    time = []
+    for param in parameters:
+        vals = getDatas("GeneralAttack_results.csv",param)
+        for x in vals:
+            time.append(float(x[3]))
+            mean.append(float(x[5]))
+            down.append(float(x[5])-float(x[7]))
+            up.append(float(x[5])+float(x[7]))
+        all_data.append(time)
+        all_data.append(mean)
+        all_data.append(up)
+        all_data.append(down)
+        var_values.append(all_data)
+        time = []
+        mean = []
+        up = []
+        down = []
+        all_data = []
+    for i in range(0,len(var_values)):
+        #plt.plot(var_values[i][0],var_values[i][1], label=parameters[i])
+        plt.plot(var_values[i][0],var_values[i][2] , label=parameters[i])
+        #plt.plot(var_values[i][0],var_values[i][3])
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.savefig("GeneralPlot", bbox_inches = 'tight', quality=95, dpi=1000, facecolor='w', edgecolor='w',
+        orientation='portrait', papertype=None, format=None,
+        transparent=False, pad_inches=0.1,
+        frameon=None, metadata=None)
 
-
-def getSimAvailableVars():
+def getAllVars():
     return ["control_alarms", "control_cameras","security_routines","safe_access",
-              "money","back_door","system_access","user_credentials","dir_credentials",
-              "safe_combination"]     
+              "money","small_money","big_money" , "back_door","system_access","user_credentials","dir_credentials",
+              "safe_combination"]   
+def getPhisicalVars():
+    return ["control_alarms", "control_cameras","security_routines","safe_access",
+              "money"] 
+    
+def getCyberVars():
+    return ["money","back_door","system_access","user_credentials","dir_credentials",
+              "safe_combination"]   
               
 def showSimAvailableVara():
-    var = getSimAvailableVars()
+    var = getAllVars()
     for x in var:
         print(x)
         
